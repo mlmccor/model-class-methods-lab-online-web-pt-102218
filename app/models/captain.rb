b6a1953ecb7b67d490052af0447a17cdf8c4d31f
Matthew McCormack <mmccormack1182@gmail.com>
2019-04-06 19:43:11 +0000

    Automatically backed up by Learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -24,6 +24,17 @@ class Captain < ActiveRecord::Base
     end
     captains
   end
+  
+  def self.sailors
+    captains = []
+    sailboat = Classification.find_by(name: 'Sailboat')
+    sailboat.boats.each do |boat|
+      if !captains.include?(boat.captain) && boat.captain
+        captains << boat.captain
+      end
+    end
+    captains
+  end
 
   def self.talented_seafarers
     Boat.all.each do |boat|
