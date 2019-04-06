822d72211f43f702d90cb8e006f72ebdbb9931f7
Matthew McCormack <mmccormack1182@gmail.com>
2019-04-06 07:40:08 -0400

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,40 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    selection = []
+    self.all.select do |boat|
+      if selection.count != 5
+        selection << boat
+      end
+    end
+    selection
+  end
+
+  def self.dinghy
+    Boat.all.select {|boat| boat.length < 20}
+  end
+
+  def self.ship
+    Boat.all.select {|boat| boat.length > 20}
+  end
+
+  def self.last_three_alphabetically
+    sorted = Boat.all.sort {|a,b| b.name <=> a.name}
+    Array.new.push(sorted[0], sorted[1], sorted[2])
+  end
+
+  def self.without_a_captain
+    Boat.all.select {|boat| !boat.captain}
+  end
+
+  def self.sailboats
+    sailboat = Classification.find_by(name: 'Sailboat')
+    Boat.all.select {|boat| boat.classifications.include?(sailboat)}
+  end
+
+  def self.with_three_classifications
+    Boat.all.select {|boat| boat.classifications.count == 3}
+  end
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,32 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+
+
+  def self.catamaran_operators
+    captains = []
+    catamaran = Classification.find_by(name: 'Catamaran')
+    catamaran.boats.each do |boat|
+      if !captains.include?(boat.captain)
+        captains << boat.captain
+      end
+    end
+    captains
+  end
+
+  def self.sailors
+    captains = []
+    sailboat = Classification.find_by(name: 'Sailboat')
+    sailboat.boats.each do |boat|
+      if !captains.include?(boat.captain) && boat.captain
+        captains << boat.captain
+      end
+    end
+    captains
+  end
+
+  def self.talented_seafarers
+    
+  end
+
 end
